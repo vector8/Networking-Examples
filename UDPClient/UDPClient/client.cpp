327ddb0c4ca3731e8bdad7a72d4e55a7d2ea1b6c
Chris <Chris@localhost>
2016-02-07 20:30:53 -0500

    Created UDP chat server and finished UDP chat client.

--- UDPClient/UDPClient/client.cpp
+++ UDPClient/UDPClient/client.cpp
@@ -2,8 +2,38 @@
 #include <iostream>
 #include <string>
 #include <WinSock2.h>
+#include <thread>
 
 #pragma comment(lib, "ws2_32.lib")
+
+SOCKET sock;
+SOCKADDR_IN sockAddr;
+int slen;
+bool running;
+
+void receive()
+{
+	char buffer[1024];
+	while (running)
+	{
+		memset(buffer, 0, 1024);
+		recvfrom(sock, buffer, 1024, 0, (struct sockaddr *) &sockAddr, &slen);
+
+		int error = WSAGetLastError();
+		if (error != WSAEWOULDBLOCK && error != 0)
+		{
+			std::cout << "Error code : " << error << "\n";
+
+			shutdown(sock, SD_SEND);
+			closesocket(sock);
+			break;
+		}
+		else if (error == 0)
+		{
+			std::cout << buffer << std::endl;
+		}
+	}
+}
 
 int main()
 {
@@ -18,7 +48,7 @@ int main()
 	}
 
 	// Create socket
-	SOCKET sock = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+	sock = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
 
 	if (sock == INVALID_SOCKET)
 	{
@@ -28,10 +58,10 @@ int main()
 	}
 
 	// Socket settings
-	SOCKADDR_IN sockAddr;
 	sockAddr.sin_port = htons(8888);
 	sockAddr.sin_family = AF_INET;
-	sockAddr.sin_addr.s_addr = inet_addr("10.20.5.169");
+	sockAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
+	slen = sizeof(sockAddr);
 
 	// non-blocking mode
 	// 0: blocking
@@ -40,6 +70,21 @@ int main()
 	ioctlsocket(sock, FIONBIO, &iMode);
 	
 	char buffer[1024];
+
+	std::string connectMsg = "0";
+	sendto(sock, connectMsg.c_str(), connectMsg.length(), 0, (struct sockaddr *) &sockAddr, slen);
+	int error = WSAGetLastError();
+	if (error != WSAEWOULDBLOCK && error != 0)
+	{
+		std::cout << "Error code : " << error << "\n";
+
+		running = false;
+		shutdown(sock, SD_SEND);
+		closesocket(sock);
+	}
+
+	running = true;
+	std::thread recThread(receive);
 	
 	// main loop
 	while (1)
@@ -47,29 +92,24 @@ int main()
 		memset(buffer, 0, 1024);
 		std::string msg;
 		std::getline(std::cin, msg);
-		int slen = sizeof(sockAddr);
+		msg = "1" + msg;
 		sendto(sock, msg.c_str(), msg.length(), 0, (struct sockaddr *) &sockAddr, slen);
 
-		int error = WSAGetLastError();
+		error = WSAGetLastError();
 
 		if (error != WSAEWOULDBLOCK && error != 0)
 		{
 			std::cout << "Error code : " << error << "\n";
 
+			running = false;
 			shutdown(sock, SD_SEND);
 			closesocket(sock);
-		}
-
-		recvfrom(sock, buffer, 1024, 0, (struct sockaddr *) &sockAddr, &slen);
-
-		if(error != WSAEWOULDBLOCK && error != 0)
-		{
-			std::cout << "Error code : " << error << "\n";
-			
-			shutdown(sock, SD_SEND);
-			closesocket(sock);
+			break;
 		}
 	}
+
+	running = false;
+	recThread.join();
 
 	// Terminate
 	WSACleanup();
