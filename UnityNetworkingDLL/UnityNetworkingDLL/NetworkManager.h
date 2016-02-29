b84472efdff85bc85bbdb828df86e9e540d6a974
Chris <vector8@users.noreply.github.com>
2016-02-28 20:20:38 -0500

    Added networking dll project.

---
+++ UnityNetworkingDLL/UnityNetworkingDLL/NetworkManager.h
@@ -0,0 +1,38 @@
+#pragma once
+
+#define _WINSOCK_DEPRECATED_NO_WARNINGS
+
+#include <winsock2.h>
+#include <thread>
+#include <string>
+
+#pragma comment(lib, "ws2_32.lib")
+
+#define BUFFLEN 1024
+
+class NetworkManager
+{
+private:
+	int port;
+	std::string serverAddress;
+	SOCKET sock;
+	struct sockaddr_in sockAddr;
+	int slen;
+	char msg[BUFFLEN];
+	bool running, isServer;
+	std::thread recThread;
+
+	void receiveThread();
+
+public:
+	NetworkManager();
+	~NetworkManager();
+
+	void initialize(int port, std::string serverAddress = "");
+
+	void throwError(std::string errorMsg);
+
+	void cleanUp();
+
+	void send(char* message, int length);
+};
---
+++ UnityNetworkingDLL/UnityNetworkingDLL/Wrapper.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include "LibSettings.h"
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+	LIB_API void runCommand(int commandType, char* arg);
+	LIB_API bool hasChanged();
+	LIB_API bool getErrorStatus();
+	LIB_API char* getErrorMessage();
+
+#ifdef __cplusplus
+}
+#endif
