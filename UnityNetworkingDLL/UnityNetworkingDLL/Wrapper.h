4ad02702e7301d4f4404cc0ca0fcaed6284709c8
Chris <Chris@localhost>
2016-03-02 01:59:42 -0500

    FUCK MY LIFE

--- UnityNetworkingDLL/UnityNetworkingDLL/Wrapper.h
+++ UnityNetworkingDLL/UnityNetworkingDLL/Wrapper.h
@@ -8,11 +8,11 @@ extern "C"
 #endif
 
 	LIB_API void initialize(int port, char* serverAddress);
-	LIB_API void sendMsg(char* msg, int msgLength);
+	LIB_API void sendMsg(char* msg);
 	LIB_API bool hasReceived();
 	LIB_API char* getLastReceived();
 	LIB_API bool hasError();
-	LIB_API const char* getError();
+	LIB_API char* getError();
 	LIB_API void cleanUp();
 
 #ifdef __cplusplus
